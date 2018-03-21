2e88093fc143678887e9c5dba232bd079046d87e
Glitch (timestampt-microservice-feddle) <none>
2018-03-21 15:44:13 +0000

    1.0.0

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-// server.js
-// where your node app starts
 
-// init project
 const express = require('express')
 const app = express()
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
 
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
+app.get("/:time", (req, res) => {
+  res.set('Content-Type', "application/json"); 
+  var date_obj = {"unix": null, "natural": null};
+  var time_int = Number.parseInt(req.params.time);
+  var time_date = new Date(req.params.time);
+  var unix_date;
+  var natural_date;
+  
+  if(time_int) {
+    unix_date = time_int;
+    natural_date = new Date(time_int * 1000);
+    natural_date = natural_date.toDateString();
+  }
+  else if(time_date != "Invalid Date"){
+    natural_date = time_date.toDateString();
+    unix_date = Date.parse(natural_date) / 1000;
+  }
+  
+  if(unix_date) date_obj.unix = unix_date;
+  if(natural_date) date_obj.natural = natural_date;
+  
+  res.send(JSON.stringify(date_obj));  
 })
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
